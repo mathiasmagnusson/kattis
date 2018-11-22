5763d9cd006b5fbb41fc37ac7cb671ac73980218
Mathias Magnusson <mathiasmagnussons@gmail.com>
2018-11-22 22:53:49 +0100

    use vectors of enums instead of other things like strings manually get the binary int that increases instead of format instead of simulating the swapping, use brainz (or copy from robin)

--- 01seq.rs
+++ 01seq.rs
@@ -1,54 +1,66 @@
 use std::io::{self, BufRead};
 
+#[derive(PartialEq, Clone, Copy)]
+enum Char {
+	Zero,
+	One,
+	Quest,
+}
+
 fn main() {
 	let stdin = io::stdin();
-	let line = stdin.lock().lines().next().unwrap().unwrap();
+	let line: Vec<Char> = stdin.lock().lines().next().unwrap().unwrap().chars().map(|c| match c { '0' => Char::Zero, '1' => Char::One, '?' => Char::Quest, _ => panic!("") }).collect();
 
 	let mut inversions = 0;
-	for_every_possibility(&line, |mut seq| {
-		loop {
-			let mut swapped = false;
-			for i in 0..(seq.len() - 1) {
-				if seq[i] == '1' && seq[i + 1] == '0' {
-					seq[i    ] = '0';
-					seq[i + 1] = '1';
-					inversions = (inversions + 1) % 1_000_000_007;
-					swapped = true;
-				}
+	for_every_possibility(&line, |seq: Vec<Char>| {
+		let mut finishes = 0;
+
+		for i in (0..seq.len()).rev() {
+			if seq[i] == Char::One {
+				inversions += seq.len() - (i + 1) - finishes;
+				finishes += 1;
 			}
-			if !swapped { break; }
 		}
 	});
 
-	println!("{}", inversions);
+	println!("{}", inversions % 1_000_000_007);
 }
 
-// Improve this:
-fn for_every_possibility<F>(line: &str, mut callback: F)
-	where F: FnMut(Vec<char>) -> () {
+fn for_every_possibility<F>(line: &Vec<Char>, mut callback: F)
+	where F: FnMut(Vec<Char>) -> () {
 	
 	let mut q_indices = vec![];
-	for (i, c) in line.chars().enumerate() {
-		if c == '?' {
+	for (i, c) in line.iter().enumerate() {
+		if *c == Char::Quest {
 			q_indices.push(i);
 		}
 	}
 
 	if q_indices.len() == 0 {
-		callback(line.split("").filter_map(|s| s.chars().next()).collect());
+		callback(line.to_vec());
 		return;
 	}
 
-	for i in 0..2u32.pow(q_indices.len() as u32) {
-		let mut s: Vec<char> = format!("{:b}", i).split("").filter_map(|s| s.chars().next()).collect();
-		while s.len() < q_indices.len() {
-			s.insert(0, '0');
-		}
-		let mut seq: Vec<char> = line.split("").filter_map(|s| s.chars().next()).collect();
+	let mut s: Vec<Char> = vec![Char::Zero; q_indices.len()];
+	loop {
+		let mut seq: Vec<Char> = line.to_vec();
 		for j in 0..s.len() {
 			seq[q_indices[j]] = s[j];
 		}
 
 		callback(seq);
+
+		if s.iter().all(|c| *c == Char::One) {
+			break;
+		}
+
+		for i in (0..s.len()).rev() {
+			if s[i] == Char::Zero {
+				s[i] = Char::One;
+				break;
+			} else {
+				s[i] = Char::Zero;
+			}
+		}
 	}
 }
