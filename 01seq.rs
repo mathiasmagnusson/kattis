8a82ec7998e88907b138ad58131d7bfdefe31354
Mathias Magnusson <mathiasmagnussons@gmail.com>
2018-11-22 23:15:54 +0100

    stop copying over the things everywhere, references worked just fine stop asking "are we there yet" after every turn

--- 01seq.rs
+++ 01seq.rs
@@ -12,12 +12,12 @@ fn main() {
 	let line: Vec<Char> = stdin.lock().lines().next().unwrap().unwrap().chars().map(|c| match c { '0' => Char::Zero, '1' => Char::One, '?' => Char::Quest, _ => panic!("") }).collect();
 
 	let mut inversions = 0;
-	for_every_possibility(&line, |seq: Vec<Char>| {
+	for_every_possibility(&line, |seq: &[Char]| {
 		let mut finishes = 0;
 
-		for i in (0..seq.len()).rev() {
-			if seq[i] == Char::One {
-				inversions += seq.len() - (i + 1) - finishes;
+		for i in 0..seq.len() {
+			if seq[i] == Char::Zero {
+				inversions += i - finishes;
 				finishes += 1;
 			}
 		}
@@ -26,8 +26,8 @@ fn main() {
 	println!("{}", inversions % 1_000_000_007);
 }
 
-fn for_every_possibility<F>(line: &Vec<Char>, mut callback: F)
-	where F: FnMut(Vec<Char>) -> () {
+fn for_every_possibility<F>(line: &[Char], mut callback: F)
+	where F: FnMut(&[Char]) -> () {
 	
 	let mut q_indices = vec![];
 	for (i, c) in line.iter().enumerate() {
@@ -37,29 +37,28 @@ fn for_every_possibility<F>(line: &Vec<Char>, mut callback: F)
 	}
 
 	if q_indices.len() == 0 {
-		callback(line.to_vec());
+		callback(&line.to_vec());
 		return;
 	}
 
 	let mut s: Vec<Char> = vec![Char::Zero; q_indices.len()];
-	loop {
+	'outer: loop {
 		let mut seq: Vec<Char> = line.to_vec();
 		for j in 0..s.len() {
 			seq[q_indices[j]] = s[j];
 		}
 
-		callback(seq);
+		callback(&seq);
 
-		if s.iter().all(|c| *c == Char::One) {
-			break;
-		}
-
-		for i in (0..s.len()).rev() {
+		'inner: for i in (0..s.len()).rev() {
 			if s[i] == Char::Zero {
 				s[i] = Char::One;
 				break;
 			} else {
 				s[i] = Char::Zero;
+				if i == 0 {
+					break 'outer;
+				}
 			}
 		}
 	}
